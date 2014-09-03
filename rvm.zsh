5268e1bad85f31ce179957f41948399586977122
paraqles <paraqles@dthal.net>
2014-09-03 15:40:48 +0200

    Changed path add to zsh style

--- rvm.zsh
+++ rvm.zsh
@@ -1,9 +1,14 @@
 ZSH_PLUGIN_RVM_DIR=$ZSH_PLUGINS_DIR/rvm
 
-PATH=$HOME/.rvm/bin:$PATH # Add RVM to PATH for scripting
+export RVM_DIR=$HOME/.rvm
+
+if [[ ! $PATH =~ "$HOME/.rvm/bin" && -d $RVM_DIR ]]; then
+  path=($RVM_DIR/bin "$path[@]") # Add RVM to PATH for scripting
+fi
 
 if [[ -x $HOME/.rvm/scripts/rvm ]]; then
   . $HOME/.rvm/scripts/rvm
 fi
 
 source $ZSH_PLUGIN_RVM_DIR/aliases.zsh
+source $ZSH_PLUGIN_RVM_DIR/functions.zsh
