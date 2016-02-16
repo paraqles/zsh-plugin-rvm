d5cff149b135bfd43d8d8211cb7a4b5432d5aa26
paraqles <paraqles@dthal.net>
2016-02-16 09:49:48 +0100

    CHANGE: rvm bin path to end of path list

--- rvm.zsh
+++ rvm.zsh
@@ -1,7 +1,7 @@
 export RVM_DIR=$HOME/.rvm
 
 if [[ ! $PATH =~ "$HOME/.rvm/bin" && -d $RVM_DIR ]]; then
-  path=($RVM_DIR/bin "$path[@]") # Add RVM to PATH for scripting
+  export path=( "$path[@]" $RVM_DIR/bin ) # Add RVM to PATH for scripting
 fi
 
 if [[ -x $HOME/.rvm/scripts/rvm ]]; then
